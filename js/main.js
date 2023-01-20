fd64aad008d65162c30fba92048f7954a0cde106
Ahmeeed22 <78164396+Ahmeeed22@users.noreply.github.com>
2023-01-20 20:34:13 +0400

    refactor

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ var swiper = new Swiper(".mySwiper", {
    $(window).on('load',()=>{
 
     $("#spinner").css('display','none').css('zIndex','-111111111111').promise().done(function(){
-      $('body').css('overflow-y', 'auto').css('overflow-x', 'hidden').css('width','100vw')
+      $('body').css('overflow-y', 'auto')
       wow = new WOW(
         {
         boxClass:     'wow',      // default
