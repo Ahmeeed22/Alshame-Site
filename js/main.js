6207748abfb11f194d093112f1dfda8014e81591
Ahmeeed22 <ahmeeedzezo33@gmail.com>
2023-09-23 17:40:13 +0400

    edit team section

--- js/main.js
+++ js/main.js
@@ -83,3 +83,242 @@ var swiper = new Swiper(".mySwiper", {
   //   let aa=[]
   //   aa.push(date)
   //  console.log(aa);
+
+
+
+
+  // Function to save the object recursively
+  let arrr=[]
+  let _id=1;
+const saveObject = async (obj, parentId = null) => {
+  
+  const newObj = new ObjectModel({
+    level: obj.level,
+    titleFr: obj.titleFr,
+    titleEn: obj.titleEn,
+    sortId: obj.sortId,
+    createdAt: obj.createdAt,
+    updatedAt: obj.updatedAt,
+    isGeneral: obj.isGeneral,
+    id: obj.id,
+    parentId: parentId,
+  });
+  // await newObj.save();
+
+  if (obj.groups && obj.groups.length > 0) {
+    for (const group of obj.groups) {
+      await saveObject(group, newObj._id);
+    }
+  }
+
+  if (obj.phraces && obj.phraces.length > 0) {
+    for (const phrase of obj.phraces) {
+      await saveObject(phrase, newObj._id);
+    }
+  }
+};
+let cc=  {
+  "level": "category",
+  "titleFr": "Motif de consultation",
+  "titleEn": "Consultation reason",
+  "sortId": 0,
+  "createdAt": "2022-11-10T16:25:32.130Z",
+  "updatedAt": "2023-07-03T06:51:28.357Z",
+  "isGeneral": true,
+  "id": "636d25fcd96056fe170005c4",
+  "groups":
+      [ 
+        {
+        "parentId": "636d25fcd96056fe170005c4",
+        "level": "group",
+        "colIndex": 2,
+        "titleFr": "Pédiatrique",
+        "titleEn": "Pediatric",
+        "sortId": 1,
+        "createdAt": "2022-11-10T16:31:18.912Z",
+        "updatedAt": "2023-07-03T06:51:28.357Z",
+        "isGeneral": true,
+        "id": "636d2756d96056fe17000600",
+        "phraces":[  {
+                      "parentId": "636d2756d96056fe17000600",
+                      "level": "sentence",
+                      "titleFr": "Évaluation audiologique à la demande de l'école",
+                      "titleEn": "Audiological exam requested by the school",
+                      "sortId": 1,
+                      "createdAt": "2022-11-10T16:31:57.534Z",
+                      "updatedAt": "2023-07-03T06:51:28.357Z",
+                      "isGeneral": true,
+                      "id": "636d277dd96056fe17000610"
+                    },
+                    {
+                      "parentId": "636d2756d96056fe17000600",
+                      "level": "sentence",
+                      "titleFr": "Retard de langage",
+                      "titleEn": "Language delay",
+                      "sortId": 4,
+                      "createdAt": "2022-11-10T16:31:38.268Z",
+                      "updatedAt": "2023-07-03T06:51:28.357Z",
+                      "isGeneral": true,
+                      "id": "636d276ad96056fe17000608"
+                    },
+                    {
+                      "parentId": "636d2756d96056fe17000600",
+                      "level": "sentence",
+                      "titleFr": "Hx d'otites à répétition",
+                      "titleEn": "Hx of repeated otitis",
+                      "sortId": 5,
+                      "createdAt": "2022-11-10T16:32:15.466Z",
+                      "updatedAt": "2023-07-03T06:51:28.357Z",
+                      "isGeneral": true,
+                      "id": "636d278fd96056fe17000618"
+                    },
+                    {
+                      "parentId": "636d2756d96056fe17000600",
+                      "level": "sentence",
+                      "titleFr": "À la demande des parents",
+                      "titleEn": "Requested by the parents",
+                      "sortId": 2,
+                      "createdAt": "2022-11-10T16:32:06.440Z",
+                      "updatedAt": "2023-07-03T06:51:28.357Z",
+                      "isGeneral": true,
+                      "id": "636d2786d96056fe17000614"
+                    },
+                    {
+                      "parentId": "636d2756d96056fe17000600",
+                      "level": "sentence",
+                      "titleFr": "Dépistage néonatal",
+                      "titleEn": "Neonatal screening",
+                      "sortId": 0,
+                      "createdAt": "2022-11-10T16:31:29.882Z",
+                      "updatedAt": "2023-07-03T06:51:28.357Z",
+                      "isGeneral": true,
+                      "id": "636d2761d96056fe17000604"
+                    },
+                    {
+                      "parentId": "636d2756d96056fe17000600",
+                      "level": "sentence",
+                      "titleFr": "Doute parentale sur l'audition",
+                      "titleEn": "Parents concerned about hearing",
+                      "sortId": 3,
+                      "createdAt": "2022-11-10T16:31:46.894Z",
+                      "updatedAt": "2023-07-03T06:51:28.357Z",
+                      "isGeneral": true,
+                      "id": "636d2772d96056fe1700060c"
+                    }
+                  ]
+        }, 
+        {
+          "parentId": "636d25fcd96056fe170005c4",
+          "level": "group",
+          "colIndex": 1,
+          "titleFr": "Suivi",
+          "titleEn": "Follow-up",
+          "sortId": 1,
+          "createdAt": "2022-11-10T16:27:45.916Z",
+          "updatedAt": "2023-07-03T06:51:28.357Z",
+          "isGeneral": true,
+          "id": "636d2681d96056fe170005e4",
+          "phraces":[  
+                      {
+                        "parentId": "636d2681d96056fe170005e4",
+                        "level": "sentence",
+                        "titleFr": "Suivi pour acouphène droit / gauche",
+                        "titleEn": "Right / Left tinnitus follow-up",
+                        "sortId": 2,
+                        "createdAt": "2022-11-10T16:29:28.845Z",
+                        "updatedAt": "2023-07-03T06:51:28.357Z",
+                        "isGeneral": true,
+                        "id": "636d26e8d96056fe170005f4"
+                      },
+                      {
+                        "parentId": "636d2681d96056fe170005e4",
+                        "level": "sentence",
+                        "titleFr": "Suivi pour Hx de liquide rétro-tympanique",
+                        "titleEn": "Follow-up for known middle ear effusion",
+                        "sortId": 5,
+                        "createdAt": "2022-11-10T16:30:07.165Z",
+                        "updatedAt": "2023-07-03T06:51:28.357Z",
+                        "isGeneral": true,
+                        "id": "636d270fd96056fe170005fc"
+                      }
+                        
+                    ]
+        },  {
+            "parentId": "636d25fcd96056fe170005c4",
+            "level": "group",
+            "colIndex": 0,
+            "titleFr": "Général",
+            "titleEn": "General",
+            "sortId": 1,
+            "createdAt": "2022-11-10T16:26:04.844Z",
+            "updatedAt": "2023-07-03T06:51:28.357Z",
+            "isGeneral": true,
+            "id": "636d261cd96056fe170005c8",
+            "phraces":[  
+                        {
+                          "parentId": "636d261cd96056fe170005c8",
+                          "level": "sentence",
+                          "titleFr": "Hypoacousie subjective à l'oreille droite / gauche",
+                          "titleEn": "Right / Left Subjective hearing loss",
+                          "sortId": 2,
+                          "createdAt": "2022-11-10T16:26:43.580Z",
+                          "updatedAt": "2023-07-03T06:51:28.357Z",
+                          "isGeneral": true,
+                          "id": "636d2643d96056fe170005d4"
+                        },
+                        {
+                          "parentId": "636d261cd96056fe170005c8",
+                          "level": "sentence",
+                          "titleFr": "À la demande de l'ORL",
+                          "titleEn": "ENT request",
+                          "sortId": 3,
+                          "createdAt": "2022-11-10T16:27:23.750Z",
+                          "updatedAt": "2023-07-03T06:51:28.357Z",
+                          "isGeneral": true,
+                          "id": "636d266bd96056fe170005e0"
+                        }
+                        
+                    ]
+        }
+      ]
+}
+
+// Function to save the object recursively
+const saveObjectnew = async (obj, parentId = null) => {
+  const newObj = new ObjectModel({
+    level: obj.level,
+    titleFr: obj.titleFr,
+    titleEn: obj.titleEn,
+    sortId: obj.sortId,
+    createdAt: obj.createdAt,
+    updatedAt: obj.updatedAt,
+    isGeneral: obj.isGeneral,
+    id: obj.id,
+    parentId: parentId,
+  });
+
+  await newObj.save();
+
+  if (obj.groups && obj.groups.length > 0) {
+    for (const group of obj.groups) {
+      await saveObject(group, newObj._id);
+    }
+  }
+
+  if (obj.phraces && obj.phraces.length > 0) {
+    for (const phrase of obj.phraces) {
+      const newPhrase = new ObjectModel({
+        level: phrase.level,
+        titleFr: phrase.titleFr,
+        titleEn: phrase.titleEn,
+        sortId: phrase.sortId,
+        createdAt: phrase.createdAt,
+        updatedAt: phrase.updatedAt,
+        isGeneral: phrase.isGeneral,
+        id: phrase.id,
+        parentId: newObj._id,
+      });
+      await newPhrase.save();
+    }
+  }
+};
